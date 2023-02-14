347e74981ca8b4dd51e5124be10c2a3226b573e4
Firewood <jmj6352@naver.com>
2023-02-14 23:28:40 +0900

    test2
    test

---
+++ studyswift.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+/*
+ // 변수,상수와 if/else
+ var greeting = 10 //var:변수, let:상수
+
+// if else문
+var value : String = greeting == 10 ? "10이다" : "10이 아니다"
+print("value:",value)
+
+if (value == "10이 아니다")
+{
+    print("not 10")
+}
+else
+{
+    print("10")
+}
+*/
+
+//배열 array
+//콜렉션:데이터를 모아둔 것, 배열,셋,딕셔너리,튜플
+var  myArray : [String] = ["가","나","다","라"]
+var  myArray2 : [Int] = [1,2,3,4,5,6,7,8]
+
+print(myArray)
+print(myArray2)
+print("github test")
